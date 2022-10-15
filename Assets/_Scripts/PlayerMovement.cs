1c4f1351cb786508b5c26770b24747615e81ebe7
JatoMixo <jatomixo@outlook.es>
2022-10-15 12:42:57 +0200

    main

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class PlayerMovement : MonoBehaviour{
 
     public float maxDistance;
+    public float speedReduction;
+    public float speedAmplification;
+    private Rigidbody2D rb;
+    private LineRenderer lineRenderer;
 
     void Start(){
-
+        rb = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.useWorldSpace = true;
     }
 
-    void Update(){
+    void FixedUpdate(){
+        if (Input.GetMouseButton(0)){
+            lineRenderer.enabled = true;
+            Vector3[] pos = new Vector3[2];
+
+            pos[0] = transform.position;
+            pos[1] = TakeMouseDistance();
+
+            lineRenderer.SetPositions(pos);
+        } else {
+            lineRenderer.enabled = false;
+        }
+
+
         if (Input.GetMouseButtonUp(0)){
-            GetComponent<Rigidbody2D>().AddForce(TakeMouseDistance());
+            rb.velocity += new Vector2(TakeMouseDistance().x * Time.deltaTime * speedAmplification, TakeMouseDistance().y * Time.deltaTime * speedAmplification);
+        }
+
+        if (rb.velocity.x != 0 || rb.velocity.y != 0){
+            rb.velocity -= new Vector2(rb.velocity.x/speedReduction, rb.velocity.y/speedReduction);
         }
     }
 
@@ -24,9 +49,8 @@ public class PlayerMovement : MonoBehaviour{
 
         float distance = Mathf.Abs(Mathf.Sqrt(Mathf.Pow(rest.x, 2) + Mathf.Pow(rest.y, 2)));
 
-        if (distance >= maxDistance){
-                
-            //GetComponent<Rigidbody2D>().AddForce(rest);
+        if (false){
+
         }
 
         return rest;
