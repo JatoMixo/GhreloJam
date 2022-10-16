1d84fab426ec2c40c223f93f8c870052ccf45ff2
JatoMixo <jatomixo@outlook.es>
2022-10-16 13:58:55 +0200

    main

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public PlayerMovement player;
+    public Transform hoyo;
     public bool levelComplete;
     public float increaseLightSpeed;
+    public float animationSpeed;
+    private bool ballDestroyed;
+    [Header("level system")]
+    public Scene mainMenu;
+    public GameObject winPanel;
 
     void Start(){
         player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        hoyo = GameObject.Find("Hoyo").transform;
     }
 
     void Update(){
         if (levelComplete){
-            player.light += Time.deltaTime * increaseLightSpeed;
+            player.transform.GetChild(0).localScale += new Vector3(Time.deltaTime * increaseLightSpeed, Time.deltaTime * increaseLightSpeed, 0);
+            Animation();
         }
     }
 
@@ -20,11 +28,28 @@ public class GameManager : MonoBehaviour
         Debug.Log("Level completed :)");
         player.isOnTorch = true;
         levelComplete = true;
+        player.enabled = false;
+
+        Animation();
 
         LevelMenu();
     }
 
+    private void Animation (){
+        player.transform.position = new Vector2(hoyo.position.x + Random.Range(-0.02f, 0.03f), hoyo.position.y + Random.Range(-0.02f, 0.03f));
+        if (player.transform.localScale.x > 0) player.transform.localScale -= new Vector3(Time.deltaTime * animationSpeed, Time.deltaTime * animationSpeed);
+        hoyo.GetChild(0).localScale += new Vector3(Time.deltaTime * increaseLightSpeed, Time.deltaTime * increaseLightSpeed);
+    }
+
     private void LevelMenu(){
-        Debug.Log("The menu of the level appears");
+        winPanel.SetActive(true);
     }    
+
+    public void NextButton (){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MenuButton(){
+        SceneManager.LoadScene(mainMenu.buildIndex);
+    }
 }
